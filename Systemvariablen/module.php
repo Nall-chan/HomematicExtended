af0ee96590b48a198c2246085cc0f951cddb92f7
Nall-chan <mt@neo-ami.de>
2015-05-10 17:44:40 +0200

    no message

--- Systemvariablen/module.php
+++ Systemvariablen/module.php
@@ -627,7 +627,7 @@ class HMSystemVariable extends HMBase
             throw new Exception('Wrong Datatype for ' . $VarID);
         else
         {
-            if (!$this->WriteSysVar($Parameter, utf8_decode($Value)))
+            if (!$this->WriteSysVar($Parameter, utf8_encode($Value)))
                 throw new Exception('Error on write Data ' . $VarID);
             else
             {
